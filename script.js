4e719996329ebbf72c262ca9388dc7c8301ea216
Karlo123-byte <karlo.jozic@ekonomska.edu.ba>
2025-02-22 17:24:24 +0100

    dodavanje JSa

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 let ime = "Karlo";
+let prezime = "Jozic";
+let godine = 21;
+let grad = "Mostar";
+let zanimanje = "Programer";
+
+console.log("Moje ime je", ime, prezime, "imam", godine, "godina studiram u", grad, "i zelim postati", zanimanje);
